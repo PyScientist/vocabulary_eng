dd8306c3921c7b72ea1a81f15c01fe9d3403f579
Sergei Dmitriev <sergei-dmitriev@mail.ru>
2024-05-05 14:51:55 +0300

    Add some words form movie, bot without translations for now. Add method of checking presence of the word in dictionary Traced the path to farther improvements of program

--- word_list.py
+++ word_list.py
@@ -4,29 +4,29 @@ import time
 
 class Word:
     """Class of single word, containing
-        name - the word on english;
-        translit - translation to russian,
-        it also can be provided definition,
-        and speach part attributes."""
+    :param name: The english word;
+    :param translit: Translation to russian (Can be several meanings).
+    It also can be provided definition, and speach part attributes after word instantiation"""
         
     def __init__(self, name, translit):
         self.name = name
         self.translation = translit
-        self.length = self.calc_word_lenght()
+        self.length = self.calc_word_length()
         self.spelling = self.check_spelling()
         self.definition = ''
         self.speach_part = ''
+        # Importance of the word could be between 0 and 10 where 10 is very important
+        self.importance = 5
 
     def check_spelling(self):
-        """Method to check spelling,
-        if word doesn't contain ' ' it
-        identified as correct."""        
+        """Method to check spelling, if word doesn't contain ' ' it identified as correct."""
         if self.name.find(' ') < 1:
             return 'correct'
         return 'wrong'
               
-    def calc_word_lenght(self):
-         return len(self.name) 
+    def calc_word_length(self):
+        """Calculation of the word length"""
+        return len(self.name)
 
 
 class OuterDictionary:
@@ -47,37 +47,46 @@ class OuterDictionary:
                 try:
                     self.word_dict[line.split(';')[0]] = line.split(';')[1].strip()
                 except IndexError as err:
-                    print(f'Possibly string {i+1} of outer dictionary is corrupted "{line}"')
+                    # Conditions if line looks corrupted
+                    if line.startswith('#'):
+                        pass
+                    else:
+                        print(f'Possibly string {i+1} of outer dictionary is corrupted "{line}"')
 
 
 class WordsList:
     """Class which contains methods
     and interfaces to add words and their properties to te list"""
     def __init__(self):
-        self.list_of_words =[]
-        self.dict_of_words ={}
+        self.dict_of_words = {}
         
     def add_word(self, word):
         "Add word from dict only word and set of its translations"
-        self.list_of_words.append(word)
         self.dict_of_words[word.name] = word
 
     def get_words_from_external_dict(self, outer_dict):
-        """Transfer words imported from text file (from combined dict) to word list"""
+        """Transfer words imported from text file (from prepared dict) to word list"""
         for k in outer_dict.word_dict.keys():
             self.add_word(Word(k, outer_dict.word_dict[k]))
+
     def choose_word_randomly(self):
         """Chose random word from WordList and print it"""
-        random_word_obj = self.list_of_words[random.randint(0, len(self.list_of_words)-1)]
+
+        random_word_obj = self.dict_of_words[list(self.dict_of_words.keys())[random.randint(0, len(self.dict_of_words)-1)]]
         print(random_word_obj.name)
         time.sleep(3.5)
         print(random_word_obj.translation)
-
 
     def print_all_words(self):
         """Print all words contained in WordList with their translations"""
         for key in self.dict_of_words.keys():
             print(f'{self.dict_of_words[key].name} - {self.dict_of_words[key].translation}')
+
+    def check_the_word_presence(self, word):
+        if word.strip().lower() in self.dict_of_words.keys():
+            print(f'The word "{word}" with meanings "{self.dict_of_words[word].translation}" is in dictionary')
+        else:
+            print(f'The word "{word}" is not in the list would you like to add it?')
 
 
 def main():
@@ -86,6 +95,10 @@ def main():
     word_list.get_words_from_external_dict(OuterDictionary('./words.txt'))
     # Work with word list
     word_list.choose_word_randomly()
+    word_list.check_the_word_presence('accusing')
+
+    # Need add exporting database in json object and back by serialization and deserialization
+    # Add simple interface with Tkinter / Web interface (The hard way)?
 
 
 if __name__ == '__main__':
