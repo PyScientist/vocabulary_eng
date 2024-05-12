d5bd56580b6a18c4273b924a79bced1d01adb806
Sergei Dmitriev <sergei-dmitriev@mail.ru>
2024-05-12 22:00:18 +0300

    Added db sqlite integration

---
+++ DbHandlers.py
@@ -0,0 +1,77 @@
+import sqlite3
+import os
+
+
+class DbTextAnalyser:
+    """Class contain provide object db for TextAnalyser tool with methods to manage db"""
+    def __init__(self, path='vocabulary.db'):
+        self.path = path
+        self.wTable = 'words'
+
+        self.con = None
+        self.cur = None
+        self.initiate_connection()
+        self.create_words_t()
+
+    def initiate_connection(self):
+        """Initiate connection to db"""
+        if os.path.exists(self.path) is True:
+            print("db exists")
+        if os.path.exists(self.path) is False:
+            print("db is not exists, create db")
+        self.con = sqlite3.connect(self.path)
+        self.cur = self.con.cursor()
+        print(F'connected to {self.con}')
+
+    def close_db_connection(self):
+        """Close connection to db"""
+        self.con.close()
+
+    def create_words_t(self):
+        # Spending tables creation
+        sql = (F"create table if not exists {self.wTable} (id, name, speach_part,"
+               F"translations, definition, importance, topic)")
+        self.cur.execute(sql)
+
+    def add_record_words_t(self, name, speach_part, translation, definition, importance, topic):
+        """Add record to words table"""
+        sql = F"SELECT COUNT(*) as count FROM {self.wTable}"
+        self.cur.execute(sql)
+        item_id = self.cur.fetchone()[0] + 1
+        sql = (F"INSERT INTO {self.wTable} VALUES ({item_id}, '{name}', '{speach_part}',"
+               F"'{translation}', '{definition}', '{importance}', '{topic}')")
+        self.cur.execute(sql)
+        self.con.commit()
+
+    def remove_record_words_t_by_id(self, item_id):
+        """Remove record from words table by given id"""
+        sql = F"DELETE FROM {self.wTable} WHERE id = {int(item_id)}"
+        self.cur.execute(sql)
+        self.con.commit()
+
+    def get_columns_names(self, table):
+        """Get columns names from given table"""
+        sql = F"pragma table_info({table})"
+        self.cur.execute(sql)
+        col_names = []
+        for el in self.cur.fetchall():
+            col_names.append(el[1])
+        return col_names
+
+    def get_records(self, table):
+        """Get all records from given table"""
+        if table == 'words':
+            sql = F"SELECT * FROM {self.wTable}"
+            self.cur.execute(sql)
+            data = self.cur.fetchall()
+            return data
+
+
+if __name__ == '__main__':
+    db_text_analyser = DbTextAnalyser()
+    #db_text_analyser.add_record_words_t('fine', 'noun', 'штраф, пеня',
+    #                                    'The fine an amount of money that has to be paid as a punishment for not obeying a rule or law', 5, 'setup')
+    #print(db_text_analyser.get_records('words'))
+    #db_text_analyser.remove_record_words_t_by_id(1)
+    print(db_text_analyser.get_records('words'))
+    db_text_analyser.close_db_connection()
--- WordList.py
+++ WordList.py
@@ -1,22 +1,25 @@
 import random
 import time
+from DbHandlers import DbTextAnalyser
 
 
 class Word:
     """Class of single word, containing
     :param name: The english word;
-    :param translit: Translation to russian (Can be several meanings).
-    It also can be provided definition, and speach part attributes after word instantiation"""
+    :param translations: Translation to russian (Can be several meanings).
+    It also can be provided definition, speach part attributes after word instantiation
+    importance and related topic. Initially the highest importance set is equal 5"""
         
-    def __init__(self, name, translit):
+    def __init__(self, name, speach_part='', translations='', definition='', importance=5, topic=''):
         self.name = name
-        self.translation = translit
+        self.translations = translations
+        self.definition = definition
+        self.speach_part = speach_part
+        # Importance of the word could be between 0 and 5 where 5 is very important
+        self.importance = importance
+        self.topic = topic
         self.length = self.calc_word_length()
         self.spelling = self.check_spelling()
-        self.definition = ''
-        self.speach_part = ''
-        # Importance of the word could be between 0 and 10 where 10 is very important
-        self.importance = 5
 
     def check_spelling(self):
         """Method to check spelling, if word doesn't contain ' ' it identified as correct."""
@@ -29,7 +32,7 @@ class Word:
         return len(self.name)
 
 
-class OuterDictionary:
+class OuterDictionary_txt:
     """Class which provides tools for import of outer dictionary with following structure:
     english word; russian translation1, russian translation2, russian translation3 ..."""
     def __init__(self, file_path=None, err_reporting=False):
@@ -59,16 +62,18 @@ class WordsList:
     """Class which contains methods
     and interfaces to add words and their properties to te list"""
     def __init__(self):
-        self.dict_of_words = {}
+        self.dict_of_words = dict()
+        # Open connection to database
+        self.db_text_analyser = DbTextAnalyser()
         
     def add_word(self, word):
-        "Add word from dict only word and set of its translations"
+        """Add word from dict only word and set of its translations"""
         self.dict_of_words[word.name] = word
 
     def get_words_from_external_dict(self, outer_dict):
         """Transfer words imported from text file (from prepared dict) to word list"""
-        for k in outer_dict.word_dict.keys():
-            self.add_word(Word(k, outer_dict.word_dict[k]))
+        for name in outer_dict.word_dict.keys():
+            self.add_word(Word(name=name, translations=outer_dict.word_dict[name]))
 
     def choose_word_randomly(self):
         """Chose random word from WordList and print it"""
@@ -76,27 +81,59 @@ class WordsList:
         random_word_obj = self.dict_of_words[list(self.dict_of_words.keys())[random.randint(0, len(self.dict_of_words)-1)]]
         print(f'Please recall this word: {random_word_obj.name}')
         time.sleep(3.5)
-        print(f'The word has following translation to Russian: {random_word_obj.translation}\n\n\n')
+        print(f'The word has following translation to Russian: {random_word_obj.translations}\n\n\n')
 
     def print_all_words(self):
         """Print all words contained in WordList with their translations"""
         for key in self.dict_of_words.keys():
-            print(f'{self.dict_of_words[key].name} - {self.dict_of_words[key].translation}')
+            print(f'{self.dict_of_words[key].name} - {self.dict_of_words[key].translations}')
 
     def check_the_word_presence(self, word):
         if word.strip().lower() in self.dict_of_words.keys():
-            print(f'The word "{word}" with meanings "{self.dict_of_words[word].translation}" is in dictionary')
+            print(f'The word "{word}" with meanings "{self.dict_of_words[word].translations}" is in dictionary')
         else:
             print(f'The word "{word}" is not in the list would you like to add it?')
 
+    def load_words_from_dict_to_db(self):
+        for key in self.dict_of_words.keys():
+            self.db_text_analyser.add_record_words_t(self.dict_of_words[key].name,
+                                                     self.dict_of_words[key].speach_part,
+                                                     self.dict_of_words[key].translations,
+                                                     self.dict_of_words[key].definition,
+                                                     self.dict_of_words[key].importance,
+                                                     self.dict_of_words[key].topic,
+                                                     )
+
+    def get_words_from_db(self):
+        table_name = 'words'
+        self.dict_of_words = dict()
+
+        # Look how organised columns in db
+        columns_names_num_dict = {}
+        for i, name in enumerate(self.db_text_analyser.get_columns_names(table_name)):
+            columns_names_num_dict[name] = i
+
+        # Upload words into memory
+        for word in self.db_text_analyser.get_records(table_name):
+            self.add_word(Word(name=word[columns_names_num_dict['name']],
+                               speach_part=word[columns_names_num_dict['speach_part']],
+                               translations=word[columns_names_num_dict['translations']],
+                               definition=word[columns_names_num_dict['definition']],
+                               importance=word[columns_names_num_dict['importance']],
+                               topic=word[columns_names_num_dict['topic']]))
 
 def main():
     word_list = WordsList()
     # Get words from outer dictionary
-    word_list.get_words_from_external_dict(OuterDictionary('./texts/vocabulary.txt', err_reporting=False))
+    #word_list.get_words_from_external_dict(OuterDictionary_txt('./texts/vocabulary.txt', err_reporting=False))
     # Work with word list
+
+    #word_list.load_words_from_dict_to_db()
+    word_list.get_words_from_db()
+
+
     word_list.choose_word_randomly()
-    word_list.check_the_word_presence('accusing')
+    #word_list.check_the_word_presence('accusing')
 
     # Need add exporting database in json object and back by serialization and deserialization
     # Add functionality to add word in dictionary, delete word from dictionary
