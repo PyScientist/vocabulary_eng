17c3d86f79b0005c7cb4cb44ea2afb8bd048ea98
Sergei Dmitriev <sergei-dmitriev@mail.ru>
2024-05-19 17:42:26 +0300

    Change salt

--- config.py
+++ config.py
@@ -1,4 +1,10 @@
 import os
 
+# The absolute path of the directory where the program resides.
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
 class Config:
-  SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'gues_what'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+
